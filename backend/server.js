4f263269ef1706736ac5bc79b264d746f3c6bc5d
loankbt <loankbt@gmail.com>
2020-07-16 18:43:40 +0700

    changes package.json

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+mongoose.connect(uri || 'mongodb://localhost/mangaweb', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 
 const connection = mongoose.connection;
